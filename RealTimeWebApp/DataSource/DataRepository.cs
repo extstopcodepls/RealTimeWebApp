d463d01ada640e1316742174263c6c076015f93f
extstopcodepls <bory12@gmail.com>
2013-12-19 12:36:10 +0100

    fix ids in data

--- RealTimeWebApp/DataSource/DataRepository.cs
+++ RealTimeWebApp/DataSource/DataRepository.cs
@@ -14,12 +14,12 @@ namespace RealTimeWebApp.DataSource
 			Products = new List<Product>
 			{
 				new Product { Id = 1, Name = "Suszarka", Desc = "Opis suszarki", Price = "999,99 Zł", UpVote = 0, DownVote = 0},
-				new Product { Id = 1, Name = "Pralka", Desc = "Opis pralki", Price = "999,99 Zł", UpVote = 0, DownVote = 0},
-				new Product { Id = 1, Name = "Widelec", Desc = "Opis widelca", Price = "999,99 Zł", UpVote = 0, DownVote = 0},
-				new Product { Id = 1, Name = "Nóż", Desc = "Opis noża", Price = "999,99 Zł", UpVote = 0, DownVote = 0},
-				new Product { Id = 1, Name = "A Nuż", Desc = "Opis a nuż", Price = "999,99 Zł", UpVote = 0, DownVote = 0},
-				new Product { Id = 1, Name = "Talerzyk", Desc = "Opis talerzyka", Price = "999,99 Zł", UpVote = 0, DownVote = 0},
-				new Product { Id = 1, Name = "Miseczka", Desc = "Opis miseczki", Price = "999,99 Zł", UpVote = 0, DownVote = 0}
+				new Product { Id = 2, Name = "Pralka", Desc = "Opis pralki", Price = "999,99 Zł", UpVote = 0, DownVote = 0},
+				new Product { Id = 3, Name = "Widelec", Desc = "Opis widelca", Price = "999,99 Zł", UpVote = 0, DownVote = 0},
+				new Product { Id = 4, Name = "Nóż", Desc = "Opis noża", Price = "999,99 Zł", UpVote = 0, DownVote = 0},
+				new Product { Id = 5, Name = "A Nuż", Desc = "Opis a nuż", Price = "999,99 Zł", UpVote = 0, DownVote = 0},
+				new Product { Id = 6, Name = "Talerzyk", Desc = "Opis talerzyka", Price = "999,99 Zł", UpVote = 0, DownVote = 0},
+				new Product { Id = 7, Name = "Miseczka", Desc = "Opis miseczki", Price = "999,99 Zł", UpVote = 0, DownVote = 0}
 			};
 		}
 	}
